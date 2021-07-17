23468297a22623ac5566756ea2e247a7ac932676
Cyril Sobierajewicz <sobierajewicz.cyril@gmail.com>
2021-07-17 20:08:15 +0200

    Convert foreign modules to try bundling with esbuild

--- src/Data/DateTime/Instant.js
+++ src/Data/DateTime/Instant.js
@@ -8,11 +8,11 @@ var createDateTime = function (y, m, d, h, mi, s, ms) {
   return dateTime;
 };
 
-exports.fromDateTimeImpl = function (y, mo, d, h, mi, s, ms) {
+export var fromDateTimeImpl = function (y, mo, d, h, mi, s, ms) {
   return createDateTime(y, mo - 1, d, h, mi, s, ms).getTime();
 };
 
-exports.toDateTimeImpl = function (ctor) {
+export var toDateTimeImpl = function (ctor) {
   return function (instant) {
     var dt = new Date(instant);
     return ctor (dt.getUTCFullYear())(dt.getUTCMonth() + 1)(dt.getUTCDate())(dt.getUTCHours())(dt.getUTCMinutes())(dt.getUTCSeconds())(dt.getUTCMilliseconds());
